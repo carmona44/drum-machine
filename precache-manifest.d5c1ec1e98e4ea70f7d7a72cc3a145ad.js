3d5d3cf7c7a7e88fa23e67449223222a29d08b20
carmona44 <daniel_95ca@hotmail.com>
2018-12-18 21:02:33 +0100

    Updates

---
+++ precache-manifest.d5c1ec1e98e4ea70f7d7a72cc3a145ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3399999d14c61ad309c6",
+    "url": "/drum-machine/static/js/runtime~main.3399999d.js"
+  },
+  {
+    "revision": "5e0c3bf1b9f32cd2391e",
+    "url": "/drum-machine/static/js/main.5e0c3bf1.chunk.js"
+  },
+  {
+    "revision": "846e098a801d8cb491f7",
+    "url": "/drum-machine/static/js/1.846e098a.chunk.js"
+  },
+  {
+    "revision": "5e0c3bf1b9f32cd2391e",
+    "url": "/drum-machine/static/css/main.835a5fbc.chunk.css"
+  },
+  {
+    "revision": "07c8290a8552515618c7701cad1285d8",
+    "url": "/drum-machine/index.html"
+  }
+];
